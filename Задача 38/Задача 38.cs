02e5ea6d181a4249e4722e833f6ee3a61eb3b29c
lightlana <lebendaufdensternen@gmail.com>
2022-10-09 22:57:16 +0300

    Задача 38:Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.

---
+++ Задача 38/Задача 38.cs
@@ -0,0 +1,59 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+void Zadacha34()
+{
+    int size = 10;
+    double[] numbers = new double[size];
+    FillArray(numbers);
+    PrintArray(numbers);
+    PoiskMinMax(numbers);
+}
+
+void PoiskMinMax(double[] numbers)
+{
+    double max = numbers[0];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Максимальное число " + max);
+
+    double min = numbers[0];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine("Минимальное число " + min);
+    Console.WriteLine();
+    Console.WriteLine($"Разница: {max} - {min} = {max - min}");
+}
+
+void FillArray(double[] numbers)
+{
+    Random random = new Random();
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = random.NextDouble();
+    }
+}
+
+void PrintArray(double[] numbers)
+{
+    Console.WriteLine("вывод массива");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Math.Round(numbers[i], 2);
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Zadacha34();
+
